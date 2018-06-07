eed8bf538ee43de50b68d9284d0fbfc380492110
ljr <962415287@qq.com>
2018-06-07 11:04:45 +0800

    ?

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ struct edge{
     double weight;
     int next;
 }E[300000];
+string oldstring[300000];
 int tote = 0;
 int totn = 0;
 void addedge(string x,string y,int typex,int typey,double weight1,double weight2){
@@ -55,61 +56,24 @@ vector <int> ansG;
 queue < pair<int,int> > Q;
 const int deep = 6;
 int in[300000];
-
-void solve(string x,int anstype){
+void solveinit(){
     ansB.clear();
     ansG.clear();
     sorceboard.clear();
     memset(in,0,sizeof(in));
     inG.clear();
-    int idx = ID[x];
     while (!Q.empty())Q.pop();
-    Q.push(make_pair(idx,0));
-    in[idx] = 1;
-    sorceboard[idx] = 100;
-    int nowdeep = 0;
-    int tmp = 0;
-    while (nowdeep <= deep && !Q.empty()){
-        pair <int,int> now = Q.front();
-        nowdeep = now.second;
-        int u = now.first;
-        Q.pop();
-        for (int i = head[u].head;i != -1 ; i = E[i].next){
-            int v = E[i].t;
-            if (in[v] == 1) continue;
-            if (nowdeep == 0) tmp++;
-            if (now.second == 0 && head[v].type == anstype && inG.count(v) == 0){
-                inG[v] = 1;
-                ansG.push_back(v);
-            }
-            if (sorceboard.count(v) == 0){
-                if (head[v].type == anstype) ansB.push_back(v);
-                sorceboard[v] = sorceboard[u] * E[i].weight;
-            }
-            else {
-                sorceboard[v] += sorceboard[u] * E[i].weight;
-            }
-            if (in[v] == 0){
-                Q.push(make_pair(v,nowdeep+1));
-                in[v] = 1;
-            }
-        }
-    }
-    int ansnum = 0;
-    for (int i = 0; i < ansG.size()&& ansnum <3 ; i++){
-        if (head[ansG[i]].s == x) continue;
+}
+void solveprintf(int ansnum ,string x,int anstype){
+    // ansG the real
+    // ansB the ans
+    for (int i = 0; i < ansG.size(); i++){
         sorceboard[ansG[i]] = -1;
-        /*
-        if (ansnum == 0) cout <<",";
-        else cout <<" ";
-        cout <<head[ansG[i]].s;
-        ansnum++;
-        */
     }
     sort(ansB.begin(),ansB.end(),cmp);
     for (int i = 0 ; i < ansB.size() && ansnum <3 ; i++){
         if (head[ansB[i]].type == anstype) {
-            if (head[ansB[i]].s == x) continue;
+            if (inG.count(ansB[i]) != 0 ) continue;
             if (ansnum == 0) cout <<",";
             else cout <<" ";
             cout <<head[ansB[i]].s;
@@ -119,9 +83,9 @@ void solve(string x,int anstype){
     if (ansnum == 0){
         cout << ",";
         while (1){
-            int x = rand() % totn + 1;
-            if (head[x].type == anstype){
-                cout << head[x].s;
+            int ggr = rand() % totn + 1;
+            if (head[ggr].type == anstype && sorceboard[ggr]>-0.5){
+                cout << head[ggr].s;
                 break;
             }
 
@@ -129,13 +93,116 @@ void solve(string x,int anstype){
     }
     cout << endl;
 }
+void solve(string x,int anstype){
+    solveinit();
+    int idx = ID[x];
+    Q.push(make_pair(idx,0));
+    in[idx] = 1;
+    sorceboard[idx] = 100;
+    int nowdeep = 0;
+    while (nowdeep <= deep && !Q.empty()){
+        pair <int,int> now = Q.front();
+        nowdeep = now.second;
+        int u = now.first;
+        Q.pop();
+        for (int i = head[u].head;i != -1 ; i = E[i].next){
+            int v = E[i].t;
+            if (in[v] == 1) continue;
+            if (now.second == 0 && head[v].type == anstype && inG.count(v) == 0){
+                inG[v] = 1;
+                ansG.push_back(v);
+            }
+            if (sorceboard.count(v) == 0){
+                if (head[v].type == anstype) ansB.push_back(v);
+                sorceboard[v] = 0;
+            }
+            sorceboard[v] += sorceboard[u] * E[i].weight;
+
+            if (in[v] == 0){
+                Q.push(make_pair(v,nowdeep+1));
+                in[v] = 1;
+            }
+        }
+    }
+    solveprintf(0,x,anstype);
+}
+vector<int> ansS;
+void solvep_in_f(string x,int anstype){
+    int ansnum = 0;
+    ansS.clear();
+    int idx = ID[x];
+    for (int i = head[idx].head ; i != -1 ; i = E[i].next){
+        int v = E[i].t;
+
+    }
+
+    in[idx] = 1;
+    solveinit();
+    sorceboard[idx] = 100;
+    int nowdeep = 0;
+    while (nowdeep <= deep && !Q.empty()){
+        pair <int,int> now = Q.front();
+        nowdeep = now.second;
+        int u = now.first;
+        Q.pop();
+        for (int i = head[u].head;i != -1 ; i = E[i].next){
+            int v = E[i].t;
+            if (in[v] == 1) continue;
+            if (now.second == 0 && head[v].type == anstype && inG.count(v) == 0){
+                inG[v] = 1;
+                ansG.push_back(v);
+            }
+            if (sorceboard.count(v) == 0){
+                if (head[v].type == anstype) ansB.push_back(v);
+                sorceboard[v] = 0;
+            }
+            sorceboard[v] += sorceboard[u] * E[i].weight;
+            if (in[v] == 0){
+                Q.push(make_pair(v,nowdeep+1));
+                in[v] = 1;
+            }
+        }
+    }
+    solveprintf(0,x,anstype);
+}
+void solvepwa(string x,int anstype){
+    solveinit();
+    int idx = ID[x];
+    Q.push(make_pair(idx,0));
+    in[idx] = 1;
+    sorceboard[idx] = 100;
+    int nowdeep = 0;
+    while (nowdeep <= deep && !Q.empty()){
+        pair <int,int> now = Q.front();
+        nowdeep = now.second;
+        int u = now.first;
+        Q.pop();
+        for (int i = head[u].head;i != -1 ; i = E[i].next){
+            int v = E[i].t;
+            if (in[v] == 1) continue;
+            if (head[u].type == 1 && head[v].type == 1) continue;
+
+            if (now.second == 0 && head[v].type == anstype && inG.count(v) == 0){
+                inG[v] = 1;
+                ansG.push_back(v);
+            }
+            if (sorceboard.count(v) == 0){
+                if (head[v].type == anstype) ansB.push_back(v);
+                sorceboard[v] = 0;
+            }
+            sorceboard[v] += sorceboard[u] * E[i].weight;
+
+            if (in[v] == 0){
+                Q.push(make_pair(v,nowdeep+1));
+                in[v] = 1;
+            }
+        }
+    }
+    solveprintf(0,x,anstype);
+}
 int main()
 {
-
-
-
     freopen("train.csv","r",stdin);
-
     string s;
     cin >> s;
     int xx = 0;
@@ -158,25 +225,31 @@ int main()
             addedge(tmp1,tmp2,1,2,0.99,0.99);
         }
         else if (relat == "paper_publish_on"){
-            addedge(tmp1,tmp2,1,5,0.88,0.88);
+            addedge(tmp1,tmp2,1,5,0.9,0.9);
         }
         else if (relat == "paper_cit_paper"){
-            addedge(tmp1,tmp2,1,1,0.88,0.1);
+            addedge(tmp1,tmp2,1,1,0.9,0.1);
         }
         else if (relat == "work_in"){
-            addedge(tmp1,tmp2,2,4,0.99,0.99);
+            addedge(tmp1,tmp2,2,4,0.9,0.9);
         }
         else if (relat == "author_is_in_field"){
-            addedge(tmp1,tmp2,2,3,0.88,0.88);
+            addedge(tmp1,tmp2,2,3,0.9,0.9);
         }
         else if (relat == "field_is_part_of"){
-            addedge(tmp1,tmp2,3,3,0.1,0.88);
+            addedge(tmp1,tmp2,3,3,0.9,0.9);
         }
         else {
             cout << relat << endl;
         }
     }
+
     cout << 2 * xx << endl;
+    cin.clear();
+    fclose(stdin);
+    freopen("oldgood.csv","r",stdin);
+    int told = 0;
+    while (getline(cin,oldstring[told++]));
     cin.clear();
     fclose(stdin);
     freopen("test1.csv","r",stdin);
@@ -184,12 +257,12 @@ int main()
     string sk;
     cin >> sk;
     int cas = 0;
-    cout << sk;
+
     freopen("ans.csv","w",stdout);
     cout << "QueryId,ExpectedTail" << endl;
     while (cin >> s) {
         cas++;
-        cout << cas ;
+
         int i = 0;
         string tmp1 = "" , relat = "", ans = "";
         while (s[i] != ',') i++;
@@ -201,28 +274,40 @@ int main()
             relat += s[i];
 
         if (relat == "paper_is_in_field"){
-            solve(tmp1,3);
+            //cout << cas ;
+            //solve(tmp1,3);
+            //solve(tmp1,3);
+            cout << oldstring[cas] << endl;
         }
         else if (relat == "paper_is_written_by"){
-            solve(tmp1,2);
+            cout << cas ;
+            solvepwa(tmp1,2);
+            //cout << oldstring[cas] << endl;
         }
         else if (relat == "paper_publish_on"){
-            solve(tmp1,5);
+            //cout << cas ;
+            //solve(tmp1,5);
+            cout << oldstring[cas] << endl;
         }
         else if (relat == "paper_cit_paper"){
-            solve(tmp1,1);
+            //cout << cas ;
+            //solve(tmp1,1);
+            cout << oldstring[cas] << endl;
         }
         else if (relat == "work_in"){
-            solve(tmp1,4);
+            //cout << cas ;
+            //solve(tmp1,4);
+            cout << oldstring[cas] << endl;
         }
         else if (relat == "author_is_in_field"){
-            solve(tmp1,3);
+            //cout << cas ;
+            //solve(tmp1,3);
+            cout << oldstring[cas] << endl;
         }
         else if (relat == "field_is_part_of"){
-            solve(tmp1,3);
-        }
-        else {
-            cout << relat << endl;
+            //cout << cas ;
+            //solve(tmp1,3);
+            cout << oldstring[cas] << endl;
         }
     }
     return 0;
